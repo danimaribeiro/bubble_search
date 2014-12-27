ec9d2fb15e8132b2c907dae62f514897d98f3ecc
Danimar Ribeiro <danimaribeiro@gmail.com>
2014-12-27 00:14:02 -0200

    First version of app

---
+++ BubbleDownloadYoutube/DataModel/YoutubeDataSource.cs
@@ -0,0 +1,201 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Storage;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The data model defined by this file serves as a representative example of a strongly-typed
+// model.  The property names chosen coincide with data bindings in the standard item templates.
+//
+// Applications may use this model as a starting point and build on it, or discard it entirely and
+// replace it with something appropriate to their needs. If using this model, you might improve app 
+// responsiveness by initiating the data loading task in the code behind for App.xaml when the app 
+// is first launched.
+
+namespace BubbleDownloadYoutube.Data
+{
+    public enum Estado
+    {
+        Aguardando = 0,
+        Downloading = 1,
+        Finalizado = 2,
+        Erro = 3
+    }
+
+    /// <summary>
+    /// Generic item data model.
+    /// </summary>
+    public class DownloadItem : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DownloadItem(String uniqueId, String titulo, String imagePath, String descricao, String content)
+        {
+            this.UniqueId = uniqueId;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.ImagePath = imagePath;
+            this.Content = content;
+            this.UrlDownload = "https://www.youtube.com/watch?v=" + uniqueId;
+            this.Status = Estado.Aguardando;
+        }
+
+        public string UniqueId { get; private set; }
+        public string Titulo { get; private set; }
+        public string Duracao { get; private set; }
+        public string Descricao { get; private set; }
+        public string ImagePath { get; private set; }
+        public string Content { get; private set; }
+        public string UrlDownload { get; private set; }
+
+        private Estado status;
+        public Estado Status
+        {
+            get { return status; }
+            set
+            {
+                status = value;
+                OnPropertyChanged("Cor");
+            }
+        }
+
+        public string Cor
+        {
+            get
+            {
+                if (status == Estado.Erro)
+                    return "red";
+                else if (status == Estado.Downloading)
+                    return "lightblue";
+                else if (status == Estado.Finalizado)
+                    return "green";
+                else
+                    return "transparent";
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.Titulo;
+        }
+    }
+
+    /// <summary>
+    /// Generic group data model.
+    /// </summary>
+    public class DownloadGrupos
+    {
+        public DownloadGrupos(String uniqueId, String titulo, String descricao)
+        {
+            this.UniqueId = uniqueId;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Items = new ObservableCollection<DownloadItem>();
+        }
+
+        public string UniqueId { get; private set; }
+        public string Titulo { get; private set; }
+        public string Descricao { get; private set; }
+        public ObservableCollection<DownloadItem> Items { get; private set; }
+
+        public override string ToString()
+        {
+            return this.Titulo;
+        }
+    }
+
+    /// <summary>
+    /// Creates a collection of groups and items with content read from a static json file.
+    /// 
+    /// SampleDataSource initializes with data read from a static json file included in the 
+    /// project.  This provides sample data at both design-time and run-time.
+    /// </summary>
+    public sealed class YoutubeDataSource
+    {
+        private static YoutubeDataSource _sampleDataSource = new YoutubeDataSource();
+
+        private ObservableCollection<DownloadGrupos> _groups = new ObservableCollection<DownloadGrupos>();
+        public ObservableCollection<DownloadGrupos> Groups
+        {
+            get { return this._groups; }
+        }
+
+        public static async Task<IEnumerable<DownloadGrupos>> GetGroupsAsync(string consulta)
+        {
+            await _sampleDataSource.SearchYoutubeVideosAsync(consulta);
+
+            return _sampleDataSource.Groups;
+        }
+
+        public static async Task<DownloadGrupos> GetGroupAsync(string uniqueId, string consulta)
+        {
+            await _sampleDataSource.SearchYoutubeVideosAsync(consulta);
+            // Simple linear search is acceptable for small data sets
+            var matches = _sampleDataSource.Groups.Where((group) => group.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        public static async Task<DownloadItem> GetItemAsync(string uniqueId, string consulta)
+        {
+            await _sampleDataSource.SearchYoutubeVideosAsync(consulta);
+            // Simple linear search is acceptable for small data sets
+            var matches = _sampleDataSource.Groups.SelectMany(group => group.Items).Where((item) => item.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+        
+        private async Task SearchYoutubeVideosAsync(string consulta)
+        {
+            this.Groups.Clear();
+
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyA4MHKy150YypY9LzPQNXTCvEgteDrBL8U",
+                ApplicationName = this.GetType().ToString()
+            });
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = consulta; // Replace with your search term.
+            searchListRequest.MaxResults = 20;
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+
+            DownloadGrupos group = new DownloadGrupos("Group-1",
+                                                            "Resultados", "Resultados da pesquisa");
+
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                switch (searchResult.Id.Kind)
+                {
+                    case "youtube#video":
+                        group.Items.Add(new DownloadItem(searchResult.Id.VideoId,
+                                                  searchResult.Snippet.Title,
+                                                  searchResult.Snippet.Thumbnails.Default.Url,
+                                                  searchResult.Snippet.Description,
+                                                  ""));
+
+                        break;
+                }
+            }
+            this.Groups.Add(group);
+        }
+    }
+}
---
+++ BubbleDownloadYoutube/Youtube/VideoDownloader.cs
@@ -0,0 +1,100 @@
+﻿using BubbleDownloadYoutube.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Networking.BackgroundTransfer;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using YoutubeExtractor;
+
+namespace BubbleDownloadYoutube.Youtube
+{
+    /// <summary>
+    /// Provides a method to download a video from YouTube.
+    /// </summary>
+    public class VideoDownloader : Downloader
+    {
+        private CancellationTokenSource cts;
+
+        public DownloadItem ItemDownload { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VideoDownloader"/> class.
+        /// </summary>
+        /// <param name="video">The video to download.</param>
+        /// <param name="savePath">The path to save the video.</param>
+        /// <param name="bytesToDownload">An optional value to limit the number of bytes to download.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="video"/> or <paramref name="savePath"/> is <c>null</c>.</exception>
+        public VideoDownloader(VideoInfo video, DownloadItem item)
+            : base(video)
+        {
+            ItemDownload = item;
+        }
+        /// <summary>
+        /// Occurs when the downlaod progress of the video file has changed.
+        /// </summary>
+        public event EventHandler<ProgressEventArgs> DownloadProgressChanged;
+        /// <summary>
+        /// Starts the video download.
+        /// </summary>
+        /// <exception cref="IOException">The video file could not be saved.</exception>
+        /// <exception cref="WebException">An error occured while downloading the video.</exception>
+        public override async void Execute()
+        {
+            try
+            {                
+                this.OnDownloadStarted(EventArgs.Empty);
+                var url = new System.Uri(this.Video.DownloadUrl);
+
+                StorageFile destinationFile
+                  = await KnownFolders.VideosLibrary.CreateFileAsync(this.Video.Title + ".mp4",
+                      CreationCollisionOption.GenerateUniqueName);
+
+                var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync(url);
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    var file = await response.Content.ReadAsByteArrayAsync();
+
+                    Windows.Storage.Streams.IRandomAccessStream stream = await destinationFile.OpenAsync(FileAccessMode.ReadWrite);
+                    IOutputStream output = stream.GetOutputStreamAt(0);
+
+                    DataWriter writer = new DataWriter(output);
+                    writer.WriteBytes(file);
+                    await writer.StoreAsync();
+                    await output.FlushAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                this.OnDownloadFinished(EventArgs.Empty);
+            }
+            this.OnDownloadFinished(EventArgs.Empty);
+        }
+
+        private void DownloadProgress(DownloadOperation download)
+        {
+            double percent = 100;
+            if (download.Progress.TotalBytesToReceive > 0)
+            {
+                percent = download.Progress.BytesReceived * 100 / download.Progress.TotalBytesToReceive;
+            }
+            if (this.DownloadProgressChanged != null)
+                this.DownloadProgressChanged(this, new ProgressEventArgs(percent));
+
+            if (percent>= 100)
+            {
+                download = null;
+            }
+        }
+
+    }
+
+
+}
